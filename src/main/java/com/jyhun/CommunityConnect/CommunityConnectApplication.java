20b73512c8fbfa63d609d339f7cc4e72bae289ad
j0hun <dudgns3681@gmail.com>
2024-04-17 11:49:01 +0900

    Initial Commit

---
+++ src/main/java/com/jyhun/CommunityConnect/CommunityConnectApplication.java
@@ -0,0 +1,13 @@
+package com.jyhun.CommunityConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyhun/CommunityConnect/CommunityConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhun.CommunityConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
